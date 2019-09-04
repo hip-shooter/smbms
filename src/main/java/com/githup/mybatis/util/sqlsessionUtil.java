88b26f4ce4a57185276c02d6a09f2d72a14eb936
YOU m <3208328792@qq.com>
2019-09-04 13:55:35 +0800

    Default Changelist

---
+++ src/main/java/com/githup/mybatis/util/sqlsessionUtil.java
@@ -0,0 +1,30 @@
+package com.githup.mybatis.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class sqlsessionUtil {
+    //私有化构造
+    private sqlsessionUtil(){};
+    //提供静态的获取sessionUtil的实例
+    public static SqlSessionFactory sqlSessionFactory;
+    //使用单例模式
+    public static SqlSessionFactory getSqlSessionFactoryInstence() throws IOException {
+        if(sqlSessionFactory==null){
+                InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        }
+        return sqlSessionFactory;
+    }
+    public static SqlSession getSqlsession()throws  IOException{
+        return getSqlSessionFactoryInstence().openSession(false);
+    }
+    public static  void  closeSqlSession(SqlSession sqlSession){
+        sqlSession.close();
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,37 @@
+import com.githup.mybatis.pojo.Provider;
+import com.githup.mybatis.pojo.User;
+import com.githup.mybatis.util.sqlsessionUtil;
+import org.apache.ibatis.session.SqlSession;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Test {
+public static void main(String[] args) {
+	InputStream is;
+	try {
+		SqlSession sqlSession = sqlsessionUtil.getSqlsession();
+		//执行配置文件中的sql语句  填命名空间.方法名(查询次数)
+		int count = sqlSession.selectOne("userMapper.findCountUser");
+		
+		//执行查询用户的操作  获取list类型的列表
+		List<User> selectList = sqlSession.selectList("userMapper.findUser");
+		for (User user : selectList) {
+			System.out.println(user);
+		}
+		 
+		//查询供应商
+		List<Provider> selectList2 = sqlSession.selectList("userMapper.findprovider");
+		for (Provider provider : selectList2) {
+			System.out.println(provider);
+		}
+		System.out.println("超市会员总共有："+count);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+}
+}
